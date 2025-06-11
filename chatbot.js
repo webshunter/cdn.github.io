baa506484c6e4bb3a6cc57e4abaf747efbb5bb42
Gugus Darmayanto <78706454+webshunter@users.noreply.github.com>
2025-06-11 12:37:37 +0700

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -445,7 +445,6 @@ class ChatBot {
                     // Send message to webhook
                     const res = await fetch("https://hook.gugusdarmayanto.my.id/webhook/0a4ca5b0-3d99-43d8-abca-ad792570f670", {
                         method: "POST",
-                        credentials: "include",
                         body: JSON.stringify([{
                             sessionId: this.currentSessionId,
                             action: "sendMessage",
